e05d9a09632679ffb77e8dde65517de3bd4dda7f
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-10-25 16:48:13 +0200

    output cluster IPs

---
+++ nomad-aws/outputs.tf
@@ -0,0 +1,11 @@
+output "consul_server_public_ip" {
+  value = ["${aws_instance.consul_server.*.public_ip}"]
+}
+
+output "nomad_server_public_ip" {
+  value = ["${aws_instance.nomad_server.*.public_ip}"]
+}
+
+output "nomad_docker_client_public_ip" {
+  value = ["${aws_instance.nomad_docker_client.*.public_ip}"]
+}
