642b109ccbc23989ad0e7deea30d128b39cdacf1
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-10-27 11:56:40 +0200

    enable docker at boot!

--- nomad-aws/ami/setup-nomadclient.sh
+++ nomad-aws/ami/setup-nomadclient.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-consulimage=${1:-"consul:1.0.0"}
 nomadimage=${1:-"beevamariorodriguez/nomad:v0.6.3"}
 
-docker pull "$consulimage"
 docker pull "$nomadimage"
-
-docker create --name=consul \
-    -v consul:/consul/data \
-    --net=host \
-    --restart=always \
-    "$consulimage" \
-    agent \
-    -retry-join 'provider=aws tag_key=consul tag_value=poc-nomad-consul' \
-    -bind '{{ GetInterfaceIP "eth0" }}'
 
 docker create --name=nomad \
     --net=host \
