a00091631e2068aa9f0b75fb34dee83ea39ae764
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-10-30 10:22:09 +0100

    fix dns entries

--- nomad-aws/dns.tf
+++ nomad-aws/dns.tf
@@ -12,9 +12,9 @@ resource "aws_route53_record" "consul" {
   records = ["${aws_instance.consul_server.*.private_ip}"]
 }
 
-resource "aws_route53_record" "nomad" {
+resource "aws_route53_record" "server" {
   zone_id = "${aws_route53_zone.private.zone_id}"
-  name    = "nomad"
+  name    = "server"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.nomad_server.*.private_ip}"]
@@ -22,7 +22,7 @@ resource "aws_route53_record" "nomad" {
 
 resource "aws_route53_record" "client" {
   zone_id = "${aws_route53_zone.private.zone_id}"
-  name    = "nomad"
+  name    = "client"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.nomad_docker_client.*.private_ip}"]
