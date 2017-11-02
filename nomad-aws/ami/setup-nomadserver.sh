aba8fbdde03f77ae954f1cdfafbf5e07ed59cd37
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-11-02 09:38:23 +0100

    nomad 0.7.0
    https://www.hashicorp.com/blog/hashicorp-nomad-0-7

--- nomad-aws/ami/setup-nomadserver.sh
+++ nomad-aws/ami/setup-nomadserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nomadversion=${1:-"0.6.3"}
+nomadversion=${1:-"0.7.0"}
 cd /tmp
 wget "https://releases.hashicorp.com/nomad/${nomadversion}/nomad_${nomadversion}_linux_amd64.zip"
 unzip nomad_${nomadversion}_linux_amd64.zip
