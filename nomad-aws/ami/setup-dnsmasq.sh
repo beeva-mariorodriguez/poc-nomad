aa4776b112c10b006a4fd33185a4ffcdc8eaf5e0
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2017-12-13 17:38:40 +0100

    run andyshinn/dnsmasq in nomad clients
    so we can send *.service.consul queries to consul, and all other to the
    VPC DNS

---
+++ nomad-aws/ami/setup-dnsmasq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+image=${1:-"andyshinn/dnsmasq:2.78"}
+
+docker pull "$image"
+
+docker create --name=dnsmasq \
+    --net=host \
+    --restart=always \
+    --cap-add NET_ADMIN \
+    "$image" \
+    -S /service.consul/127.0.0.1#8600 --log-facility=- -za 172.17.0.1
+
