d3185c7d36636e608f6696235266b732432d97fd
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2017-12-14 17:25:56 +0100

    more outputs

--- nomad-aws/outputs.tf
+++ nomad-aws/outputs.tf
@@ -2,6 +2,22 @@ output "bastion_public_ip" {
   value = ["${aws_instance.bastion.*.public_ip}"]
 }
 
+output "consul_server_public_ip" {
+  value = ["${aws_instance.consul_server.*.public_ip}"]
+}
+
+output "nomad_server_public_ip" {
+  value = ["${aws_instance.nomad_server.*.public_ip}"]
+}
+
+output "nomad_docker_client_public_ip" {
+  value = ["${aws_instance.nomad_docker_client.*.public_ip}"]
+}
+
+output "vault_server_public_ip" {
+  value = ["${aws_instance.vault_server.*.public_ip}"]
+}
+
 output "lb_dns_name" {
   value = "${aws_elb.lb.dns_name}"
 }
