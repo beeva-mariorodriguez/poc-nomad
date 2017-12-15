77237051281afa0c29032b508cb597cc71c0ae14
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2017-12-15 11:53:53 +0100

    fix vault installation

--- nomad-aws/scripts/setup-vaultserver.sh
+++ nomad-aws/scripts/setup-vaultserver.sh
@@ -9,9 +9,11 @@ storage "consul" {
 }
 EOF
 
+private_ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
 cat << EOF | sudo tee /etc/vault.d/listener.hcl
-storage "consul" {
-    address = "127.0.0.1:8200"
+listener "tcp" {
+    address = "${private_ip}:8200"
     tls_disable = "true"
 }
 EOF
@@ -23,6 +25,7 @@ docker run -d --name=vault \
     -v /etc/vault.d:/etc/vault.d \
     --net=host \
     --restart=always \
+    -e "VAULT_ADDR=http://${private_ip}:8200"
     "$image" \
     server \
     -config '/etc/vault.d'
